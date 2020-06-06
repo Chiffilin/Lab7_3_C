3e4577e643c3dead5c3dfc79078de171e81a8b19
Chiffilin <dimaaitbaev42@gmail.com>
2020-06-06 15:52:04 +0300

    Добавьте файлы проекта.

---
+++ Lab7_3_C/Lab7_3_C.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream> 
+#include <queue>
+
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+    int n, el;
+    queue<int> q1;
+    queue<int> q2;
+    queue<int> q3;
+    cout << "Введите размер очереди:\n";
+    cout << "n = ";
+    cin >> n;
+    cout << "Введите первую очередь:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> el;
+        q1.push(el);
+    }
+    cout << "Введите вторую очередь:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> el;
+        q2.push(el);
+    }
+    for (int i = 0; i < n; i++) {
+        q3.push(q1.front());
+        q1.pop();
+        q3.push(q2.front());
+        q2.pop();
+    }
+    cout << "Результат:\n";
+    while (!q3.empty()) {
+        cout << q3.front() << " ";
+        q3.pop();
+    }
+    system("pause");
+    return 0;
+}
